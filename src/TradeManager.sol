d54f48e50d724f271d48f0880b540b8e6f9f7aaa
maxsiz <maxsizmobile@gmail.com>
2024-03-17 21:33:53 +0700

    FEAT Mint with fee

--- src/TradeManager.sol
+++ src/TradeManager.sol
@@ -15,12 +15,16 @@ contract TradeManager {
     }
 
     uint256 constant public START_PRICE = 1;         
-    uint256 constant public PRICE_INCREASE_STEP = 1; // 1 stable coin unit, not decimal. 
-    uint256 constant public INCREASE_FROM_ROUND = 1;
-    uint256 constant public ROUND_VOLUME = 1_000_000e6; // in wei
+    uint256 constant public PRICE_INCREASE_STEP = 1; // 1 decimal unit of stable coin
+    uint256 constant public INCREASE_FROM_ROUND = 0;
+    uint256 immutable public ROUND_VOLUME = 1_000_000 * 10**_distributionTokenDecimals(); // in wei
+    uint256 constant public FEE_PERCENT_POINT = 50000;
+    uint256 constant public PERCENT_DENOMINATOR = 10000;
     
     address immutable public BENEFICIARY;
     uint8 immutable public TRADE_DECIMALS;
+
+    Fee public fee;
 
     IERC20 public tradeToken;
 
@@ -34,7 +38,7 @@ contract TradeManager {
     constructor (
     	address _feeBeneficiary,
         address _tradeFor,       // e.g. USDT address
-        uint8 _tradeDecimals     // e.g. 6 for USDT
+        uint8   _tradeDecimals     // e.g. 6 for USDT
     ) 
     {
     	BENEFICIARY = _feeBeneficiary;
@@ -57,10 +61,8 @@ contract TradeManager {
     ) 
         external 
     {
-        require(
-            _calcTokensForExactStable(_inAmount) >= _outNotLess, 
-            "Slippage occur"
-        );
+        (uint256 out, ) = _calcMintTokensForExactStable(_inAmount);
+        require(out >= _outNotLess, "Slippage occur");
         mintTokensForExactStable(_inAmount); 
 
     }     
@@ -73,11 +75,14 @@ contract TradeManager {
     {
 
         // 1. Calc distribution tokens
-        uint256 outAmount = _calcTokensForExactStable(_inAmount);
+        (uint256 outAmount, uint256 inAmountFee)= _calcMintTokensForExactStable(_inAmount);
         require(outAmount > 0, 'Cant buy zero');
         
         // 2. Get payment
+        tradeToken.safeTransferFrom(msg.sender, address(this), _inAmount);
+        
         // 3. Charge Fee
+        fee.total += inAmountFee;
         // 4. Mint distribution token
         _mintFor(msg.sender, outAmount);
         emit Deal(msg.sender, address(tradeToken), _inAmount, outAmount);
@@ -92,27 +97,32 @@ contract TradeManager {
     /// get by user if he(she) pay given stable coin amount
     /// @dev _inAmount must be with given in wei (eg 1 USDT =1000000)
     /// @param _inAmount stable coin amount that user want to spend
-    function calcTokensForExactStable(uint256 _inAmount) 
+    /// @param outAmount stable coin amount that user will get
+    /// @param inAmountFee fee that will given from _inAmount
+    function calcMintTokensForExactStable(uint256 _inAmount) 
         external 
         view 
-        returns(uint256) 
+        returns(uint256 outAmount, uint256 inAmountFee) 
     {
-        return _calcTokensForExactStable(_inAmount);
+        (outAmount, inAmountFee) = _calcMintTokensForExactStable(_inAmount);
     }
 
     /// @notice Returns amount of stable coins that must be spent
     /// for user get given  amount of  token
     /// @dev _outAmount must be with given in wei (eg 1 UBDN =1e18)
     /// @param _outAmount distributing token amount that user want to get
-    function calcStableForExactTokens(uint256 _outAmount) 
+    /// @param inAmount amount of stable coins that must be spent with fee includede
+    /// @param includeFee fee amount that already included in inAmount
+    function calcMintStableForExactTokens(uint256 _outAmount) 
         external 
         view 
-        returns(uint256) 
+        returns(uint256 inAmount, uint256 includeFee) 
     {
-        return _calcStableForExactTokens(_outAmount);
+        
+        (inAmount, includeFee) =  _calcMintStableForExactTokens(_outAmount);
     }
 
-    /// @notice Returns price without decimals and distributing token rest
+    /// @notice Returns price  and distributing token rest
     /// for given round
     /// @dev returns tuple  (price, rest)
     /// @param _round round number
@@ -132,11 +142,11 @@ contract TradeManager {
 
     /////////////////////////////////////////////////////////////////////
 
-    function _calcStableForExactTokens(uint256 _outAmount) 
+    function _calcMintStableForExactTokens(uint256 _outAmount) 
         internal
         virtual 
         view 
-        returns(uint256 inAmount) 
+        returns(uint256 inAmount, uint256 includeFee) 
     {
         uint256 outA = _outAmount;
         uint256 curR = _currenRound();
@@ -155,18 +165,25 @@ contract TradeManager {
                 inAmount += outA 
                     * curPrice * 10**TRADE_DECIMALS
                     / (10**dstTokenDecimals);
-                return inAmount;
+                //return inAmount;
+                break;
             }
         }
+        // Fee Charge
+        includeFee = inAmount * FEE_PERCENT_POINT / (100 * PERCENT_DENOMINATOR);
+        inAmount += includeFee; // return inAmount with fee incleded
     }
 
-    function _calcTokensForExactStable(uint256 _inAmount) 
+    function _calcMintTokensForExactStable(uint256 _inAmount) 
         internal
         virtual 
         view 
-        returns(uint256 outAmount) 
+        returns(uint256 outAmount, uint256 inAmountFee) 
     {
-        uint256 inA = _inAmount;
+        // Calc realy inamount with excluded fee
+        uint256 inA = _inAmount * 100 * PERCENT_DENOMINATOR 
+            / (100 * PERCENT_DENOMINATOR + FEE_PERCENT_POINT);
+        inAmountFee = _inAmount - inA;
         uint256 curR = _currenRound();
         uint256 curPrice; 
         uint256 curRest;
@@ -194,7 +211,7 @@ contract TradeManager {
                 outAmount += inA 
                   * 10**dstTokenDecimals
                   / (curPrice * 10**TRADE_DECIMALS);
-                return outAmount;
+                return (outAmount, inAmountFee);
             }
         }
     }
