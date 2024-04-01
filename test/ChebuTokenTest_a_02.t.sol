dc4bd2b1348ca3f1c64e43388f8ab4336492e167
Alex <alexamuek@gmail.com>
2024-04-01 17:48:44 +0800

    add tests  with calculation

--- test/ChebuTokenTest_a_02.t.sol
+++ test/ChebuTokenTest_a_02.t.sol
@@ -9,14 +9,14 @@ import {MockERC20} from "../src/mock/MockERC20.sol";
 import {ChebuToken}  from "../src/ChebuToken.sol";
 import {TradeManager}  from "../src/TradeManager.sol";
 
-contract ChebuTokenTest_a_01 is Test {
+contract ChebuTokenTest_a_02 is Test {
 
     struct Fee {
         uint256 total;
         uint256 claimed;
     }
 
-    uint256 public inUSDTAmount = 1_000_000_000_000e6; //15 usdt
+    uint256 public inUSDTAmount = 1_000_000e6; // 1 000 000 usdt
     //string public detrustName = 'NameOfDeTrust';
     ChebuToken  public memcoin;
     MockERC20 public usdt;
@@ -33,18 +33,12 @@ contract ChebuTokenTest_a_01 is Test {
         );
     }
 
+// use huge amounts
     function test_Mint() public {
         
         usdt.transfer(address(1), inUSDTAmount);
         vm.prank(address(1));
         usdt.approve(address(memcoin), inUSDTAmount);
-
-        //buy memcoins - with revert
-        vm.prank(address(1));
-        vm.expectRevert("MAX_TOTAL_SUPPLY LIMIT");
-        memcoin.mintTokensForExactStable(inUSDTAmount);
-
-        inUSDTAmount = 100e6; //100 usdt
 
         uint256 inCleanedUSDTAmount = inUSDTAmount * 100 * memcoin.PERCENT_DENOMINATOR() / 
             (100 * memcoin.PERCENT_DENOMINATOR() + memcoin.FEE_PERCENT_POINT());
@@ -74,8 +68,6 @@ contract ChebuTokenTest_a_01 is Test {
         assertEq(inAmountFee, calculatedFee);
 
         vm.prank(address(1));
-        vm.expectEmit();
-        emit TradeManager.Deal(address(1), address(usdt), inUSDTAmount, outAmount);
         memcoin.mintTokensForExactStable(inUSDTAmount);
         assertEq(round, memcoin.getCurrentRound());
         assertEq(memcoin.totalSupply(),memcoinAmount);
@@ -86,27 +78,8 @@ contract ChebuTokenTest_a_01 is Test {
 
         assertEq(usdt.balanceOf(address(memcoin)), beforeUSDTContractBalance + inUSDTAmount);
 
-        vm.prank(address(1));
-        vm.expectRevert("Unauthorized");
-        memcoin.claimFee(calculatedFee);
-
-        vm.prank(address(this));
-        vm.expectRevert();
-        memcoin.claimFee(calculatedFee + 1);
-
-        beforeUSDTContractBalance = usdt.balanceOf(address(memcoin));
-        uint256 beforeThisBalance = usdt.balanceOf(address(this));
-        vm.prank(address(this));
-        memcoin.claimFee(calculatedFee / 10);
-        (total, claimed) = memcoin.fee();
-        assertEq(claimed, calculatedFee / 10);
-        assertEq(total, calculatedFee);
-
-        assertEq(usdt.balanceOf(address(memcoin)), beforeUSDTContractBalance - calculatedFee / 10);
-        assertEq(usdt.balanceOf(address(this)), beforeThisBalance + calculatedFee / 10);  
-
         // try to calculate memcoin amount for exact usdt amount
-        // next 100 usdt
+        // next 1 000 000 usdt
 
         inCleanedUSDTAmount = inUSDTAmount * 100 * memcoin.PERCENT_DENOMINATOR() / 
             (100 * memcoin.PERCENT_DENOMINATOR() + memcoin.FEE_PERCENT_POINT());
@@ -140,9 +113,9 @@ contract ChebuTokenTest_a_01 is Test {
         assertEq(outAmount, memcoinAmount - memcoinAmountBefore);
 
         (uint256 inAmount, ) = memcoin.calcMintStableForExactTokens(memcoinAmount - memcoinAmountBefore);
-        assertApproxEqAbs(inUSDTAmount, inAmount, 1);
+        assertApproxEqAbs(inUSDTAmount, inAmount, 3);
 
-        // buy the next part of memcoins (by 100 usdt)
+        // buy the next part of memcoins (by 1000 000 usdt)
         usdt.transfer(address(1), inUSDTAmount);
         vm.startPrank(address(1));
         usdt.approve(address(memcoin), inUSDTAmount);
@@ -152,12 +125,11 @@ contract ChebuTokenTest_a_01 is Test {
         assertEq(memcoinAmount, memcoin.totalSupply());
 
         assertEq(memcoin.balanceOf(address(1)),memcoinAmount);
-
     }
 
     function test_Burn() public {
          // calculate burn
-        inUSDTAmount = 200e6;
+        inUSDTAmount = 2_000_000e6; // 2 000 000 usdt
         usdt.transfer(address(1), inUSDTAmount);
         vm.startPrank(address(1));
         usdt.approve(address(memcoin), inUSDTAmount);
@@ -165,7 +137,7 @@ contract ChebuTokenTest_a_01 is Test {
         vm.stopPrank();
         uint256 round = memcoin.getCurrentRound();
         uint256 memcoinAmount = memcoin.totalSupply();
-        uint256 burntAmount = 2_000_000e18; // burnt amount
+        uint256 burntAmount = 200_000_000e18; // burnt amount
         uint256 restMemcoinsInLastRound =  memcoinAmount - (round - 1) * memcoin.ROUND_VOLUME();
         uint256 price = memcoin.START_PRICE() + (round - 1) * memcoin.PRICE_INCREASE_STEP();
         uint256 bAm = burntAmount - restMemcoinsInLastRound;
@@ -212,29 +184,5 @@ contract ChebuTokenTest_a_01 is Test {
         assertApproxEqAbs(usdt.balanceOf(address(1)), beforeUSDTAccBalance + outUsdtAmount, 1);
         assertApproxEqAbs(usdt.balanceOf(address(memcoin)), beforeUSDTContractBalance - outUsdtAmount, 1);
         assertEq(round, memcoin.getCurrentRound());
-
-        // very small amount to burn - expect revert
-        burntAmount = 1e16;
-        vm.startPrank(address(1));
-        memcoin.approve(address(memcoin), burntAmount);
-        vm.expectRevert("Cant buy zero");
-        memcoin.burnExactTokensForStable(burntAmount);
-        vm.stopPrank();
     } 
-
-    function test_checkSlippage() public {
-
-        // check slippage
-        inUSDTAmount = 200e6;
-        usdt.transfer(address(1), inUSDTAmount);
-        vm.startPrank(address(1));
-        usdt.approve(address(memcoin), inUSDTAmount);
-        (uint256 wishOutAmount, ) = memcoin.calcMintTokensForExactStable(inUSDTAmount);
-        vm.expectRevert('Slippage occur');
-        memcoin.mintTokensForExactStableWithSlippage(inUSDTAmount, wishOutAmount + 1);
-        memcoin.mintTokensForExactStableWithSlippage(inUSDTAmount, wishOutAmount);
-        vm.stopPrank();
-    }
-
-    
 }
